2ad2157bdeb7cd004841e36870151aada656b788
LeonidVasilyev <leonid.arbeiter@gmail.com>
2015-01-22 23:02:21 +0300

    Refactor code a bit.

--- js/humanize.js
+++ js/humanize.js
@@ -1,169 +1,176 @@
 var HumanizedEditor = (function () {
 
-    var humanizedEditor = {
-        init: function () {
-            var editor = create();
-            configure(editor);
-            update(editor);
-            // Clear first undo stack element to prevent ctrl-z from return editor to it's default value and not initial post body value.
-            clearUndoStack(editor);
-            onOriginalEditorValueChange(editor);
-        }
-    };
+	var humanizedEditor = {
+		init: function () {
+			var editor = create();
+			configure(editor);
+			update(editor);
+			// Clear first undo stack element to prevent ctrl-z from return editor to it's default value and not initial post body value.
+			clearUndoStack(editor);
+			keepUpdated(editor);
+		}
+	};
 
-    var originalEditorSelector = '#postingHtmlBox';
+	function create() {
+	    // Contains original html editor and original compose editor.
+	    var editorsContainer = $('.boxes');
+	    editorsContainer.append('<div id="humanizedEditorWrapper" class="editor htmlBoxWrapper"></div>');
+		// Option holder style change a bit later than window resize or media queries applied.
+		// It shrinks editor, which is not rerender text wrap after container div is resized.
+	    // So I add stretched to whole editors wrapper iframe and observe it's windows resize event to rerender editors text wrap. 
+		var originalEditorHolder = $('.editorHolder');
+		originalEditorHolder.append('<iframe id="holderResizeSignalFrame" width=100% height=100% style="position:absolute;z-index:-1"></iframe>');
+		// Turn off mode switching.
+		var modeToggle = $('span.tabs');
+		modeToggle.hide();
+        // Inject humanized editor.
+		var humanizedEditor = ace.edit('humanizedEditorWrapper');
+		return humanizedEditor;
+	}
 
-    function create() {
-        // Copy original post wrapper and put new editor there.
-        // TODO: replace with .after().
-        $('.boxes').append('<div id="humanizedEditorWrapper" class="editor htmlBoxWrapper"></div>');
-        // Option holder style change a bit later than window resize or media queries applied.
-        // It shrinks editor, which is not rerender text wrap after container div is resized.
-        // So I add stretched to whole editors wrapper iframe and observe it's windows resize event to rerender editors text wrap. 
-        $('.editorHolder').append('<iframe id="holderResizeSignalFrame" width=100% height=100% style="position:absolute;z-index:-1"></iframe>');
-        // Turn off mode switching.
-        var modeToggle = $('span.tabs');
-        modeToggle.hide();
+	function configure(humanizedEditor) {
+		// TODO: Check and fix html-worker loading issue.
+		humanizedEditor.getSession().setUseWorker(false);
+		humanizedEditor.getSession().setMode('ace/mode/html');
+		humanizedEditor.setTheme('ace/theme/monokai');
+		humanizedEditor.getSession().setUseWrapMode(true);
+		humanizedEditor.setFontSize(13);
+		humanizedEditor.setShowPrintMargin(false);
+		humanizedEditor.commands.addCommand({
+			name: 'insertImage',
+			bindKey: { win: 'Ctrl-I' },
+			exec: insertImage,
+			readonly: false
+		});
+		humanizedEditor.commands.addCommand({
+			name: 'beautifyPost',
+			bindKey: { win: 'Ctrl-B' },
+			exec: function () {
+				var beautifiedValue = html_beautify(humanizedEditor.getValue());
+				humanizedEditor.setValue(beautifiedValue, 1);
+			},
+			readonly: false
+		});
+		humanizedEditor.on('change', function () {
+			// Update original textarea value, which will be posted on post Update.
+		    var humanizedEditorValue = humanizedEditor.getValue();
+		    var originalEditor = $('#postingHtmlBox');
+		    originalEditor.val(humanizedEditorValue);
+		});
+		humanizedEditor.on('blur', function () {
+			updateOriginalEditorSelection(humanizedEditor);
+		});
 
-        var humanizedEditor = ace.edit('humanizedEditorWrapper');
+		var holderResizeSignalFrameWindow = $('#holderResizeSignalFrame')[0].contentWindow.window;
+		holderResizeSignalFrameWindow.onresize = function () {
+			humanizedEditor.resize(true);
+		};
+	}
 
-        return humanizedEditor;
-    }
+	function update(humanizedEditor) {
+	    var originalEditor = $('#postingHtmlBox');
+	    var originalEditorValue = originalEditor.val();
+		var humanizedEditorValue = humanizedEditor.getValue();
+		if (originalEditorValue.length > 0 && originalEditorValue !== humanizedEditorValue) {
+            // 1 - set cursor at the end of the text.
+		    humanizedEditor.setValue(originalEditorValue, 1);
+		}
+	}
 
-    function configure(humanizedEditor) {
-        // TODO: Check and fix html-worker loading issue.
-        humanizedEditor.getSession().setUseWorker(false);
-        humanizedEditor.getSession().setMode('ace/mode/html');
-        humanizedEditor.setTheme('ace/theme/monokai');
-        humanizedEditor.getSession().setUseWrapMode(true);
-        humanizedEditor.setFontSize(13);
-        humanizedEditor.setShowPrintMargin(false);
-        humanizedEditor.commands.addCommand({
-            name: 'insertImage',
-            bindKey: { win: 'Ctrl-I' },
-            exec: insertImage,
-            readonly: false
-        });
-        humanizedEditor.commands.addCommand({
-            name: 'beautifyPost',
-            bindKey: { win: 'Ctrl-B' },
-            exec: function () {
-                var beautifiedValue = html_beautify(humanizedEditor.getValue());
-                humanizedEditor.setValue(beautifiedValue, 1);
-            },
-            readonly: false
-        });
-        humanizedEditor.on('change', function () {
-            // Update original textarea value, which will be posted on post Update.
-            var humanizedEditorValue = humanizedEditor.getValue();
-            $(originalEditorSelector).val(humanizedEditorValue);
-        });
-        humanizedEditor.on('blur', function () {
-            updateOriginalEditorSelection(humanizedEditor);
-        });
-        $('#holderResizeSignalFrame')[0].contentWindow.window.onresize = function () {
-            humanizedEditor.resize(true);
-        };
-    }
-
-    function update(humanizedEditor) {
-        var originalEditorValue = $(originalEditorSelector).val();
-        var humanizedEditorValue = humanizedEditor.getValue();
-        if (originalEditorValue.length > 0 && originalEditorValue !== humanizedEditorValue) {
-            //originalEditorValue = vkbeautify.xml(originalEditorValue);
-            humanizedEditor.setValue(originalEditorValue, 1);
-        }
-    }
-
-    function onOriginalEditorValueChange(humanizedEditor) {
-        var originalEditorObserver = new MutationObserver(function (mutations) {
-            mutations.forEach(function (mutation) {
-                // TODO: Fix. This won't work in case post body is empty and in case of new post in html mode.
-                // Posts switch.
-                var originalEditorID = 'postingHtmlBox';
-                if (mutation.target.id === originalEditorID &&
+	function keepUpdated(humanizedEditor) {
+		var originalEditorObserver = new MutationObserver(function (mutations) {
+			mutations.forEach(function (mutation) {
+				// Posts switch.
+				var originalEditorID = 'postingHtmlBox';
+				if (mutation.target.id === originalEditorID &&
                         mutation.attributeName === 'disabled' &&
                         mutation.target.disabled === false) {
-                    update(humanizedEditor);
-                    // New post - new undo stack.
-                    clearUndoStack(humanizedEditor);
-                }
+					update(humanizedEditor);
+					// New post - new undo stack.
+					clearUndoStack(humanizedEditor);
+				}
 
-                // Image insert.
-                var insertImageDialogBackgroundClass = 'modal-dialog-bg';
-                if (mutation.target.className === insertImageDialogBackgroundClass &&
+				// Image insert.
+				var insertImageDialogBackgroundClass = 'modal-dialog-bg';
+				if (mutation.target.className === insertImageDialogBackgroundClass &&
                         mutation.target.nodeName === 'DIV' &&
                         mutation.attributeName === 'style' &&
                         mutation.target.style.display === 'none') {
-                    update(humanizedEditor);
-                }
-            });
+					update(humanizedEditor);
+				}
+			});
 
-        });
-        var configuration = { attributes: true, subtree: true };
-        originalEditorObserver.observe(document.body, configuration);
-    }
+		});
+		var configuration = { attributes: true, subtree: true };
+		originalEditorObserver.observe(document.body, configuration);
+	}
 
-    function updateOriginalEditorSelection(humanizedEditor) {
-        // Convert selection positions to start and end character indexes.
-        var start = humanizedEditor.session.doc.positionToIndex(humanizedEditor.selection.getRange().start)
-        var end = humanizedEditor.session.doc.positionToIndex(humanizedEditor.selection.getRange().end)
-        $(originalEditorSelector).get(0).setSelectionRange(start, end);
-    }
+	function updateOriginalEditorSelection(humanizedEditor) {
+		// Convert selection positions to start and end character indexes.
+		var start = humanizedEditor.session.doc.positionToIndex(humanizedEditor.selection.getRange().start)
+		var end = humanizedEditor.session.doc.positionToIndex(humanizedEditor.selection.getRange().end)
+        // get(0) because I need actual textarea behind original html editor to properly set selection.
+		var originalEditor = $('#postingHtmlBox').get(0);
+		originalEditor.setSelectionRange(start, end);
+	}
 
-    var UndoManager = require('ace/undomanager').UndoManager;
-    function clearUndoStack(humanizedEditor) {
-        humanizedEditor.getSession().setUndoManager(new UndoManager());
-    }
+	var UndoManager = require('ace/undomanager').UndoManager;
+	function clearUndoStack(humanizedEditor) {
+		humanizedEditor.getSession().setUndoManager(new UndoManager());
+	}
 
-    function insertImage() {
-        var insertImageTrigger = document.getElementById('imageUpload');
+	function insertImage() {
+		var insertImageTrigger = document.getElementById('imageUpload');
 
-        var mouseDown = new Event('mousedown', { "bubbles": true });
-        var mouseUp = new Event('mouseup', { "bubbles": true });
-        var mouseOut = new Event('mouseout', { "bubbles": true });
+		var mouseDown = new Event('mousedown', { "bubbles": true });
+		var mouseUp = new Event('mouseup', { "bubbles": true });
+		var mouseOut = new Event('mouseout', { "bubbles": true });
 
-        insertImageTrigger.dispatchEvent(mouseDown);
-        insertImageTrigger.dispatchEvent(mouseUp);
-        insertImageTrigger.dispatchEvent(mouseOut);
-    }
+		insertImageTrigger.dispatchEvent(mouseDown);
+		insertImageTrigger.dispatchEvent(mouseUp);
+		insertImageTrigger.dispatchEvent(mouseOut);
+	}
 
-    function switchToLabels() {
-        var labelsTrigger = $('.optionHolder a').get(1);
-        labelsTrigger.click();
-    }
-    return humanizedEditor;
+	function switchToLabels() {
+		var labelsTrigger = $('.optionHolder a').get(1);
+		labelsTrigger.click();
+	}
+
+	return humanizedEditor;
 }());
 
 var bloggerObserver = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
-        if ((mutation.target.id === 'postingHtmlBox' && mutation.attributeName === "disabled" && mutation.target.disabled === false)) {
-            // At this moment all data needed for humanized editor creation presented on the page if html mode is on by default.
-            var humanizedEditor = Object.create(HumanizedEditor);
-            humanizedEditor.init();
-            bloggerObserver.disconnect();
-        } else if (mutation.target.className === 'state composeMode') {
-            // Switch to html mode if compose mode is turned on by default.
-            var htmlModeToggle = $('span.tabs button:last-of-type()');
-            htmlModeToggle.trigger('click');
-            // Now I need to wait until blogger loads html version of a post.
-            bloggerObserver.disconnect();
-            bloggerObserver = new MutationObserver(function (mutations) {
-                mutations.forEach(function (mutation) {
-                    if (mutation.target.id === 'blogger-app' && mutation.target.nodeName === 'BODY' && mutation.type === 'childList' && mutation.removedNodes.length === 1 && mutation.removedNodes[0].className === 'gwt-PopupPanel') {
-                        // At this moment all data needed for humanized editor creation presented on the page if compose mode is on by default.
-                        var humanizedEditor = Object.create(HumanizedEditor);
-                        humanizedEditor.init();
-                        bloggerObserver.disconnect();
-                    }
-                });
-            });
-            var configuration = { subtree: true, childList: true};
-            bloggerObserver.observe(document.body, configuration);
-        }
-    });
+        // Because of the dynamic nature of Blogger website it is not easy to catch moment when everthing that I need is ready.
+        // All if-conditions are oblique signs that certain event is occur.
+		if ((mutation.target.id === 'postingHtmlBox' && mutation.attributeName === "disabled" && mutation.target.disabled === false)) {
+			// Html mode is on by default and post is fully loaded.
+			var humanizedEditor = Object.create(HumanizedEditor);
+			humanizedEditor.init();
+			bloggerObserver.disconnect();
+		} else if (mutation.target.className === 'state composeMode') {
+		    // Turns out that compose mode is on by default.
+            // Switch to html mode.
+			var htmlModeToggle = $('span.tabs button:last-of-type()');
+			htmlModeToggle.trigger('click');
+			// Now I need to wait until blogger loads html version of a post.
+			bloggerObserver.disconnect();
+			bloggerObserver = new MutationObserver(function (mutations) {
+				mutations.forEach(function (mutation) {
+					if (mutation.target.id === 'blogger-app' && mutation.target.nodeName === 'BODY' && mutation.type === 'childList' && mutation.removedNodes.length === 1 && mutation.removedNodes[0].className === 'gwt-PopupPanel') {
+						// Html version of post is fully loaded.
+						var humanizedEditor = Object.create(HumanizedEditor);
+						humanizedEditor.init();
+						bloggerObserver.disconnect();
+					}
+				});
+			});
+			var configuration = { subtree: true, childList: true };
+			bloggerObserver.observe(document.body, configuration);
+		}
+	});
 });
 
 // TODO: Add attribute filter.
-var configuration = { attributes: true, subtree: true};
+var configuration = { attributes: true, subtree: true };
 bloggerObserver.observe(document.body, configuration);
